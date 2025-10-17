1dd2515dba6130d11b9fc3473c08f35fccba16a8
Nathanael Martinez <martinez.nathanael123@gmail.com>
2025-10-17 14:24:11 +0800

    All UI for the page completed. TODO: Response Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // Icons
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { MdEmail, MdOutlinePerson, MdLink } from "react-icons/md";
 
 export default function Home() {
   const socialLinks: { icon: React.ReactNode; href: string }[] = [
@@ -22,26 +22,43 @@ export default function Home() {
 
   return (
     <>
-      <section className="w-full h-full flex items-center">
-        <article className="w-full h-fit flex flex-col justify-center items-center gap-5 ">
+      <section className="w-full h-full flex items-center font-poppins">
+        <article className="w-full h-fit flex flex-col justify-center items-center gap-7">
           {/* Links */}
-          <div className="flex items-center gap-10">
+          <div className="flex items-center gap-7">
             {socialLinks.map((social, index) => (
               <Link
                 href={social.href}
                 key={index}
                 target="_blank"
                 referrerPolicy="no-referrer"
-                className="p-4 rounded-full border-2 border-dirty-green/80 shadow-md shadow-dirty-green hover:scale-110 hover:bg-black-two/60 hover:shadow-dirty-white/80 hover:shadow transition-all ease-linear cursor-pointer"
+                className="p-3.5 rounded-full border-2 border-dirty-green/80 shadow-md shadow-dirty-green hover:scale-110 hover:bg-black-two/60 hover:shadow-dirty-white/80 hover:shadow transition-all ease-linear cursor-pointer"
               >
                 {social.icon}
               </Link>
             ))}
           </div>
           {/* Name */}
-          <h1 className="font-black-han text-5xl text-center leading-normal bg-clip-text text-transparent bg-gradient-to-r from-dirty-white to-beige uppercase">
+          <h1 className="max-w-xl font-black-han text-5xl md:text-6xl text-center leading-normal bg-clip-text text-transparent bg-gradient-to-r from-dirty-white to-beige uppercase">
             Nathanael Martinez
           </h1>
+
+          {/* Links */}
+          <div className="w-fit flex items-center gap-x-7 flex-wrap justify-center gap-y-5">
+            <Link
+              href="/about"
+              className="flex items-center text-white gap-x-3 px-2 py-3 w-48 justify-center rounded-lg shadow shadow-dirty-white bg-linear-90 from-black-two via-green-gray/50 to-green-gray font-medium md:text-lg hover:scale-105 transition-transform ease-in-out"
+            >
+              <MdOutlinePerson size={28} color="#FFFFFF" /> About Me
+            </Link>
+            <Link
+              href="https://drive.google.com/drive/folders/1l9vN3TWjthNEg_4J_iOXnWWY8hPdYO8W?usp=sharing"
+              target="_blank"
+              className="flex items-center text-white gap-x-3 px-2 py-3 w-48 justify-center rounded-lg shadow shadow-dirty-white bg-linear-90 from-black-two via-green-gray/50 to-green-gray font-medium md:text-lg hover:scale-105 transition-transform ease-in-out"
+            >
+              <MdLink size={28} color="#FFFFFF" /> CV
+            </Link>
+          </div>
         </article>
       </section>
     </>
